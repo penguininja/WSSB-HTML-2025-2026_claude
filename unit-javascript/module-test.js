7b17df77ea25a1f5c6edbb50ece0332356d77136
John Le <le.johnq@gmail.com>
2025-10-02 10:25:20 -0700

    an idea for javascript and allowing for javascript files
    * can't use modules unless the students are able to start a server
    * can do all javascript goes into a single file, which is probably fine

---
+++ unit-javascript/module-test.js
@@ -0,0 +1,6 @@
+console.log("we're doing javascript");
+alert("Welcome to my page");
+import {greeting} from './other.js';
+
+const name = prompt("What's your name?");
+alert(greeting(name));
---
+++ unit-javascript/no-module.js
@@ -0,0 +1,5 @@
+console.log("we're doing javascript");
+alert("Welcome to my page");
+
+console.log('everything in one file')
+console.log('not allowed to use imports if opening the html file directly in browser')
