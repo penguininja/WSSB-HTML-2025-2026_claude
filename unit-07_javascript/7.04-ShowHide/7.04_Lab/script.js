c6cf45dd4da3f748968275ae4e430051bf953efa
Candice Newton <candice.c.newton@gmail.com>
2026-04-20 16:29:03 -0700

    Add 7.04-7.08 initial lessons

---
+++ unit-07_javascript/7.04-ShowHide/7.04_Lab/script.js
@@ -0,0 +1,13 @@
+const button = document.querySelector('#btn');
+const details = document.querySelector('#details');
+
+function toggleDetails() {
+  // TODO: Replace ... with condition in if statement
+  if (...) {
+    // TODO: Write js here ...
+  } else {
+    //TODO: Write js here ...
+  }
+}
+
+button.addEventListener('click', toggleDetails);
